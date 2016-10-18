c145505fadd00da435d327040017972599b067c4
Rickard Berglund <rigurd@hotmail.com>
2016-10-18 23:54:03 +0200

    Second commit - Finished tests for class DataAnalysis

--- src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
@@ -1,5 +1,6 @@
 package se.nackademin.examination.examination_jacoco;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -19,5 +20,57 @@ public class DataAnalysisTest {
 				result.contains("The name of the homecity is big and the participant is 30 or older"));
 
 	}
+	
+	@Test
+	public void testResultStringNamesAndAge() {
+		DataAnalysis da = new DataAnalysis();
+		ArrayList<String> val = new ArrayList<String>();
+		ArrayList<String> val1 = new ArrayList<String>();
+		ArrayList<String> val2 = new ArrayList<String>();
+	
+		
+		val.addAll(Arrays.asList("Game", "Rafael", "Silvaaaa", "M", "30", "Fortaleza"));
+		String result = da.buildFinalString(val);
+		assertTrue(da.getResultStringNamesAndAge(val),
+				result.contains("The first name is smaller or equals in size to the last name and the participant is 30 or younger"));
 
+		val1.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "32", "Fortaleza"));
+		String result1 = da.buildFinalString(val1);
+		assertTrue(da.getResultStringNamesAndAge(val1),
+				result1.contains("The first name is greater or equals in size to the last name and the participant older than 30"));
+	
+		val2.addAll(Arrays.asList("Game", "Rafa", "Silv", "M", "32", "Fortaleza"));
+		String result2 = da.buildFinalString(val2);
+		assertTrue(da.getResultStringNamesAndAge(val2),
+				result2.contains("No analysis was performed"));
+	}
+	
+	@Test
+	public void testResultStringHomecityAndAge(){
+		DataAnalysis da = new DataAnalysis();
+		ArrayList<String> val = new ArrayList<String>();
+		ArrayList<String> val1 = new ArrayList<String>();
+		ArrayList<String> val2 = new ArrayList<String>();
+		ArrayList<String> val3 = new ArrayList<String>();
+		
+		val.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "32", "For"));
+		String result = da.buildFinalString(val);
+		assertTrue(da.getResultStringNamesAndAge(val),
+				result.contains("The name of the homecity is small and the participant is 30 or older"));
+		
+		val1.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "29", "For"));
+		String result1 = da.buildFinalString(val1);
+		assertTrue(da.getResultStringNamesAndAge(val1),
+				result1.contains("The name of the homecity is small and the participant is younger than 30"));
+		
+		val2.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "32", "Fortal"));
+		String result2 = da.buildFinalString(val2);
+		assertTrue(da.getResultStringNamesAndAge(val2),
+				result2.contains("The name of the homecity is big and the participant is 30 or older"));
+		
+		val3.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "29", "Fortal"));
+		String result3 = da.buildFinalString(val3);
+		assertTrue(da.getResultStringNamesAndAge(val3),
+				result3.contains("The name of the homecity is big and the participant is younger than 30"));
+	}
 }
